a70a60c8cdb60ae26845e1c75d8d7732cd8f340a
Peter <plasne@iname.com>
2018-10-02 14:26:35 -0400

    added portal

---
+++ dist/portal/index.js
@@ -0,0 +1,7 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const msg = document.getElementById('message');
+    if (msg) {
+        msg.innerHTML = 'or does it?';
+    }
+});
