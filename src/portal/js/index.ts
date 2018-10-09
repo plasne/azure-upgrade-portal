06c5922ce19857fed110e119d2f9384f018ee59e
mjyeaney <michael.yeaney@gmail.com>
2018-10-09 14:04:36 -0700

    Added new dist folders

---
+++ src/portal/js/index.ts
@@ -0,0 +1,10 @@
+class Application {
+    // constructor() {}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const msg = document.getElementById('message');
+    if (msg) {
+        msg.innerHTML = 'or does it?';
+    }
+});
